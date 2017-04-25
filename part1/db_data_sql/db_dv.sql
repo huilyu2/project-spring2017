cf04d8b67515fce1414b0fa732f6e0b3607d1473
yezhilengyue <asdf4862@qq.com>
2017-04-25 10:17:25 -0500

    DB_data_update

--- part1/db_data_sql/db_dv.sql
+++ part1/db_data_sql/db_dv.sql
@@ -38,6 +38,21 @@ pickup_type varchar(255),
 drop_off_type varchar(255)
 );
 
+
+create table stops(
+stop_id varchar(255),
+stop_code varchar(255),
+stop_name varchar(255),
+stop_desc varchar(255),
+stop_lat varchar(255),
+stop_lon varchar(255),
+zone_id varchar(255),
+stop_url varchar(255),
+location_type varchar(255),
+parent_station varchar(255)
+);
+
+
 load data local infile '/Users/chenyuwei/Desktop/5\ Data\ Visualization/google_transit/shapes.txt ' into table shapes
 fields terminated by ','
 ignore 1 lines;
@@ -47,6 +62,10 @@ fields terminated by ','
 ignore 1 lines;
 
 load data local infile '/Users/chenyuwei/Desktop/5\ Data\ Visualization/google_transit/stop_times.txt ' into table stop_times
+fields terminated by ','
+ignore 1 lines;
+
+load data local infile '/Users/chenyuwei/Desktop/5\ Data\ Visualization/google_transit/stops.txt ' into table stops
 fields terminated by ','
 ignore 1 lines;
 
@@ -60,7 +79,7 @@ group by shape_id);
 drop view stop_times_view;
 -- select trip_id, 
 create view stop_times_view as (
-SELECT trip_id, minute(timediff(max(time(arrival_time)), min(time(arrival_time)))) as period
+SELECT trip_id, TIME_TO_SEC(timediff(max(time(arrival_time)), min(time(arrival_time))))/60 as period
 from stop_times
 group by trip_id);
 
@@ -77,37 +96,40 @@ group by shape_id
 );
 
 -- ----------------------- I am divider ---------------------------------
+use transit;
+select * from stop_times_view;
+
+-- -----------
+
+drop view temp_1;
+create view temp_1 as(
+select shape_id, min(trip_id) as trip_id
+from trips
+group by shape_id);
+
+drop view shape_period;
+create view shape_period as(
+select temp_1.shape_id,period
+from temp_1,stop_times_view
+where temp_1.trip_id = stop_times_view.trip_id);
+
+-- select count(*) from stop_times_view;
+select * from temp_1;
+select period from shape_period;
+select * from shape_period;
+select * from shape_view;
+select * from stop_times_view;
+select shape_id from trips where trip_id = '[@15.0.63189138@][1][1338404910860]/0__Y1UIMF'  -- period = 70   shape_id = 100N
 
 
 
-select shape_len   -- works!
-from shape_view
-where shape_id = '100N';
+select * from shape_view where shape_id = '100N';
 
-select *  -- Not works !
-from  temp1
-where shape_id = '100N';
-
-
-select shape_id
-from shape_view
-
-
-select shape_id
-from temp1
-where shape_id = '100N'
-
-select -- temp1.shape_id, shape_len, trips_per_hour -- count(shape_id)  -- 677
-from temp1 join shape_view on temp1.shape_id
--- where temp1.shape_id = shape_view.shape_id;
+-- -----------!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 
-
-
-
-
-
+select * from temp_1;
 
 
 
@@ -123,184 +145,260 @@ DROP VIEW shape_times;
 create view shape_times as (
 select trips.shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence,shape_dist_traveled, 60/period as times-- distinct trips.shape_id, 60/period as times
 from trips,stop_times_view
-where trips.trip_id = stop_times_view.trip_id);  -- 5498
+where trips.trip_id = stop_times_view.trip_id);  
 
 select count(*) from shape_times;
 
+--  --------------- I am divider --------------------- -------
 
-select shape_id
-from shape_times
-group by shape_id
-having count(shape_id)>1;
 
+
+select trip_id, count(trip_id) from stop_times group by trip_id having count(trip_id) = 7;
+-- '[@2.0.86175868@][1458585713139]/19__O4_MFF'  7 stops
+ 
+select trip_id, shape_id from trips where trip_id ='[@2.0.86175868@][1458585713139]/19__O4_MF';
+-- shape_id= ‘[@2.0.86175868@]19’
+
+
+-- ahah 就选你这个shape啦
+-- 这个trip所经过的所有的stop
+select stop_id, stop_sequence from stop_times where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF'; -- 7
+
+-- 这些stop的地理位置
+drop view pass_stop_view;
+
+create view pass_stop_view as (
+select stop_id, stop_lat, stop_lon from stops 
+where stop_id  in 
+(select stop_id from stop_times where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF'));  -- 7
+
+-- 这个trip所经过的地理位置
+select shape_id, shape_pt_lat, shape_pt_lon from shapes where shape_id = '[@2.0.86175868@]19';
+
+drop view diff_stop_dist;
+create view diff_stop_dist as (
+select shapes.shape_id, stop_id, cast(shape_dist_traveled as signed) as distance, shape_pt_sequence
+from shapes, pass_stop_view 
+where shape_id = '[@2.0.86175868@]19' 
+	and shapes.shape_pt_lat = pass_stop_view.stop_lat 
+    and shapes.shape_pt_lon = pass_stop_view.stop_lon
+    );
+
+
+select stop_id, from diff_stop_dist;    
+
+-- timediff(max(time(arrival_time)), min(time(arrival_time)))
+drop view temp_diff_time_view;
+create view temp_diff_time_view as(
+select cast(stop_sequence as signed) as stop_sequence, stop_id, time(arrival_time) as starttime  -- stop_id, arrival_time-- trip_id, minute(timediff(time(arrival_time) - min(time(arrival_time))))
+from stop_times where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF' and stop_id in (
+select stop_id
+from shapes, pass_stop_view 
+where shape_id = '[@2.0.86175868@]19' 
+	and shapes.shape_pt_lat = pass_stop_view.stop_lat 
+    and shapes.shape_pt_lon = pass_stop_view.stop_lon
+    )
+);
+
+
+
+drop view distance_temp;
+create view distance_temp as(
+select stop_id, cast(shape_dist_traveled as signed) as distance
+from shapes, pass_stop_view 
+where shape_id = '[@2.0.86175868@]19' 
+	and shapes.shape_pt_lat = pass_stop_view.stop_lat 
+    and shapes.shape_pt_lon = pass_stop_view.stop_lon
+    );
+    
+
+-- select * from distance_temp;
+
+drop table tb_stop_distance;
+create table tb_stop_distance (
+seq int,
+stop_id varchar(255),
+distance int
+);
+
+
+-- insert into tb_stop_distance values(1,'BTSLR')
+-- select * from temp_diff_time_view; 
+create view ori_distance as(
+select stop_sequence, temp_diff_time_view.stop_id,distance 
+from distance_temp, temp_diff_time_view
+where temp_diff_time_view.stop_id = distance_temp.stop_id
+);
+
+-- select * from ori_distance;
+
+create view diff_stop_distance as (
+
+select d2.stop_sequence as seq2,
+		d2.stop_id as stop_id,
+        d2.distance - d1.distance as diff_distance
+from ori_distance d1, ori_distance d2
+where d1.stop_sequence + 1 = d2.stop_sequence
+order by seq2
+
+
+);
+
+
+
+
+
+create view diff_stop_times as (
+select  -- t1.stop_sequence as seq1, 
+		t2.stop_sequence as seq2,
+        -- t1.starttime,
+	    -- t2.starttime,
+        -- t1.stop_id as stop_id1, 
+        t2.stop_id as stop_id2, 
+        time_to_sec(timediff(t2.starttime, t1.starttime))/60 as difference
+        
+from temp_diff_time_view as t1, temp_diff_time_view as t2
+where t1.stop_sequence + 1 = t2.stop_sequence
+order by seq2
+);
+
+
+
+select * from diff_stop_times;
+select * from diff_stop_distance;
+
+
+select cast(stop_sequence as signed) as stop_sequence, stop_id  -- stop_id, arrival_time-- trip_id, minute(timediff(time(arrival_time) - min(time(arrival_time))))
+from stop_times where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF' and stop_id in (
+select stop_id
+from shapes, pass_stop_view 
+where shape_id = '[@2.0.86175868@]19' 
+	and shapes.shape_pt_lat = pass_stop_view.stop_lat 
+    and shapes.shape_pt_lon = pass_stop_view.stop_lon
+    )
+
+
+
+
+
+
+
+
+-- time slot of each stops
+drop view diff_stop_time;
+create view diff_stop_time as (
+select  -- t1.stop_sequence as seq1, 
+		t2.stop_sequence as seq2,
+        -- t1.starttime,
+	    -- t2.starttime,
+        -- t1.stop_id as stop_id1, 
+        t2.stop_id as stop_id2, 
+        time_to_sec(timediff(t2.starttime, t1.starttime))/60 as difference
+        
+from temp_diff_time_view as t1, temp_diff_time_view as t2
+where t1.stop_sequence + 1 = t2.stop_sequence
+order by seq2
+);
+
+
+select * from temp_diff_time_view;
+
+
+
+-- select * from diff_stop_time;
+
+-- speed for each stop slot
+drop view speed_in_stop;
+create view speed_in_stop as(
+select diff_stop_time.stop_id2 as stop_id, cast(shape_pt_sequence as signed) as shape_seq, distance/difference as miles_per_minute-- ,distance, difference as time_slot
+from diff_stop_time, diff_stop_dist
+where diff_stop_dist.stop_id = diff_stop_time.stop_id2
+);
+
+
+-- select miles_per_minute from speed_in_stop;
+
+
+create table speed_area_color(
+stop_seq int,
+stop_id varchar(255),
+diff_time float,
+diff_dist int
+);
+
+
+load data local infile '/Users/chenyuwei/Desktop/5\ Data\ Visualization/google_transit/speed_area_color.csv' into table speed_area_color
+fields terminated by ','
+ignore 1 lines;
+
+
+alter table speed_area_color add color varchar(255);
+
+select stop_seq,stop_id,diff_time,diff_dist, diff_dist/diff_time as speed
+from speed_area_color;
+
+alter table speed_area_color add speed float;
+
+update speed_area_color set speed = diff_dist/diff_time;
+
+update speed_area_color set color = '#EDF7B4' where diff_dist/diff_time <= 300;
+update speed_area_color set color = '#3180B6' where diff_dist/diff_time > 300 and diff_dist/diff_time < 600;
+update speed_area_color set color = '#82CCBB' where diff_dist/diff_time >= 600;
+
+
+select * from speed_area_color;
+
+
+-- a specific table for this shape
+create table spec_shape as (
 select * 
-from shape_times
-where shape_id = '1 YELLOW ALT 25'
-
-
-select shapes.shape_id,hape_pt_lat,shape_pt_lon,shape_pt_sequence,shape_dist_traveled,times
-from shapes, shape_times
-where shapes.shape_id = shape_times.shape_id;
-
-
-
-
-
-
-
-
-
-
-
-select *
-from shape_view, 
-
-
-
-
-select *
-from stop_times_view
-where period < 60;
-
-
-
-
-
-
-
-select trips.shape_id, shape_len
-from trips,shape_view
-where trips.shape_id = shape_view.shape_id;
-
-select *
 from shapes
-where shape_id = 'TEAL 23'
+where shape_id = '[@2.0.86175868@]19' 
+);
 
+alter table spec_shape add area int;  -- which area it belongs to
+alter table spec_shape add color varchar(255);  -- for color show
+alter table spec_shape drop color;
 
 
+--  set area code
+update spec_shape set area = 1 where cast(shape_pt_sequence as signed) <= 110;
 
+update spec_shape set area = 2 where cast(shape_pt_sequence as signed) > 110 and cast(shape_pt_sequence as signed) <= 191;
 
+update spec_shape set area = 3 where cast(shape_pt_sequence as signed) > 191 and cast(shape_pt_sequence as signed) <= 214;
 
+update spec_shape set area = 4 where cast(shape_pt_sequence as signed) > 214 and cast(shape_pt_sequence as signed) <= 340;
 
+update spec_shape set area = 5 where cast(shape_pt_sequence as signed) > 340 and cast(shape_pt_sequence as signed) <= 399;
 
+update spec_shape set area = 6 where cast(shape_pt_sequence as signed) > 399 and cast(shape_pt_sequence as signed) <= 608;
 
 
 
+-- set color for stop
+--  #6F3662 #FF7182 #FFAE5D #F8F8F2 #F8DEBD #9F6164
+-- update spec_shape set color = "#6F3662" where area = 1;
+-- update spec_shape set color = "#FF7182" where area = 2;
+-- update spec_shape set color = "#FFAE5D" where area = 3;
+-- update spec_shape set color = "#F8F8F2" where area = 4;
+-- update spec_shape set color = "#F8DEBD" where area = 5;
+-- update spec_shape set color = "#9F6164" where area = 6;
 
--- Wrong ---- 
--- select count(*)
--- from trips inner join stop_times_view on trips.trip_id;
 
+select * from stop_times_view where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF'; -- 3
+select * from shape_view where shape_id = '[@2.0.86175868@]19'; -- 1699
 
+select * from stop_times where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF';
 
-alter table trips add length numeric;
 
-update trips 
-set trips.length = (select shape_view.shape_len
-from shape_view where trips.shape_id = shape_view.shape_id);
+select * from speed_in_stop;
+-- select * from stop_times;
+select * from spec_shape;
 
 
+select route_id from trips where trip_id = '[@2.0.86175868@][1458585713139]/19__O4_MF';
 
-select shape_id
-from trips
-where exists (select shape_id, shape_len from shape_view )
 
-
-
-
-select *
-from shape_view
-group by shape_id
-having count(*) > 1;
-
-select shape_id, count(shape_id)
-from trips
-group by shape_id
-having count(*) > 1;
-
-
-select trips
-
-
-
-
-
--- -------------------
-
-select  trips.shape_id, shape_len  -- 24700
-from trips join shape_view on trips.shape_id;
-
-select distinct shape_id from trips;
-
-
-select count(*) from shape_view;  -- 677 distinct shape_id
-
-
-
-where trips.shape_id = shape_view.shape_id; 
-
-select distinct shape_id
-from shapes
-where not exists (select distinct shape_id from trips);
-
-select * from shapes where shape_id = 'TEAL 26';
-select * from trips where shape_id = 'TEAL 26'
-
-select count(*) from shape_view;   -- 677
-
-select min(shape_id) from trips;
-select * from trips where shape_id = (select min(shape_id) from trips);
-
-SELECT * from trips where shape_id in (select  shape_id from shapes);
-
-
-
--- select shape_id
--- from shape_view
--- group by shape_id
--- having count(shape_id)>1;
-
-
-select *
-from trips
-where shape_id = '1 YELLOW ALT 25'
-
-
-
-select trip_id
-from trips
-group by trip_id
-having count(trip_id) > 1;
-
-
-select shape_len from shape_view where shape_id = '1 YELLOW ALT 25'
-
-
-
--- select count(*) from trips;
-select trip_id from stop_times_view;
-
-
-select shape_id, max(cast(shape_dist_traveled as signed)) as shape_len -- 677
-from shapes
-group by shape_id;
-
-select s1.shape_id
-from shape_view s1, shape_view s2
-where s1.shape_len < s2.shape_len and s1.shape_id = s2.shape_id;
-
-select *
-from shape_view
-where shape_id = '100 LATE 3';
-
-select *
-from trips
-where shape_id not in (select shape_id from shapes)
-
-
-select count(distinct shape_id) 
-from shapes
-where shape_id not in (
-
-select count(distinct shape_id)
-from trips);
+select min(shape_pt_lat),max(shape_pt_lat),min(shape_pt_lon),max(shape_pt_lon) 
+from shapes where shape_id = '[@2.0.86175868@]19';
